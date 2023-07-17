7a16b166047b4c904011f84050f9e9e5ac16b0c6
Mike Valeriano <mkvlrn@gmail.com>
2023-07-17 15:44:27 -0300

    chore: setup prisma

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { multiply } from '#/supermath/advanced-math.js';
-
-const result = multiply(210, 2);
-
-console.log(`210 x 2 = ${result}`);
+export {};
--- src/supermath/basic-math.ts
+++
@@ -1,7 +0,0 @@
-export function sum(a: number, b: number) {
-  return a + b;
-}
-
-export function subtract(a: number, b: number) {
-  return a - b;
-}
